03b39e6b0a4faba4f519fb3c31c921585c8ae60a
Matho7 <martinluptak777@gmail.com>
2023-11-15 16:24:37 +0100

    Added file config.py to .gitignore

--- config.py
+++
@@ -1,8 +0,0 @@
-# config.py
-
-# Define your database connection parameters
-db_host = "147.175.106.248"
-db_database = "DB_luptak"
-db_user = "xluptak"
-db_password = "mArTiN."
-api_key = "AIzaSyCfJl4fQQfTmfqeME7JkZw3pE1AcoHKCIQ"
