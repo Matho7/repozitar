f00f96009703d1e7e19fc18e96fbce25e3054846
Matho7 <martinluptak777@gmail.com>
2024-03-04 13:47:35 +0100

    peewee

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ class Waypoint(BaseModel):
     id = AutoField()
     route_id = ForeignKeyField(Route, backref='waypoints')
     lat = FloatField()
-    long = FloatField()
+    lng = FloatField()
     elevation = FloatField(null=True)
     order_wp = IntegerField(constraints=[Check('order_wp > 0')])
 
@@ -60,35 +60,48 @@ class Waypoint(BaseModel):
         table_name = 'waypoints'
 
 
-db.create_tables([User, Route, Waypoint])
+db.connect()
+
+
+def conn():
+    db.create_tables([User, Route, Waypoint])
+    routes = Route.select().join(Waypoint).group_by(Route.id)
+    return routes
 
 
 class PolylineHandler(tornado.web.RequestHandler):
     def get(self, route_id):
-
+        self.set_header("Cache-Control", "no-cache, no-store, must-revalidate")
         waypoints = Waypoint.select().where(Waypoint.route_id == route_id)
 
-        m = folium.Map(location=[waypoints[0].lat, waypoints[0].long], zoom_start=6)
+        m = folium.Map(location=[waypoints[0].lat, waypoints[0].lng], zoom_start=6)
 
         for waypoint in waypoints:
-            folium.Marker([waypoint.lat, waypoint.long]).add_to(m)
+            folium.Marker([waypoint.lat, waypoint.lng]).add_to(m)
 
-        coordinates = [[waypoint.lat, waypoint.long] for waypoint in waypoints]
+        coordinates = [[waypoint.lat, waypoint.lng] for waypoint in waypoints]
 
         folium.PolyLine(locations=coordinates, color='red').add_to(m)
 
         m.save('polyline.html')
-        self.render('polyline.html')
+
+        with open('./polyline.html', 'r') as file:
+            content = file.read()
+
+        self.write(content)
 
 
 class DirectionsHandler(tornado.web.RequestHandler):
     def get(self, route_id):
+        self.set_header("Cache-Control", "no-cache, no-store, must-revalidate")
+        os.remove("./directions.html")
 
         waypoints = Waypoint.select().where(Waypoint.route_id == route_id)
-        start = (waypoints[0].lat, waypoints[0].long)
-        end = (waypoints[-1].lat, waypoints[-1].long)
 
-        intermediate_waypoints = [(waypoint.lat, waypoint.long) for waypoint in waypoints[1:-1]]
+        start = (waypoints[0].lat, waypoints[0].lng)
+        end = (waypoints[-1].lat, waypoints[-1].lng)
+
+        intermediate_waypoints = [(waypoint.lat, waypoint.lng) for waypoint in waypoints[1:-1]]
 
         url = f'http://router.project-osrm.org/route/v1/driving/{start[1]},{start[0]};'
 
@@ -113,13 +126,18 @@ class DirectionsHandler(tornado.web.RequestHandler):
 
         folium.PolyLine(locations=coordinates, color='red').add_to(m)
 
-        m.save('osrm.html')
-        self.render('osrm.html')
+        m.save('directions.html')
+
+
+        with open('./directions.html', 'r') as file:
+            content = file.read()
+
+        self.write(content)
 
 
 class RoutesHandler(tornado.web.RequestHandler):
     def get(self):
-        routes = Route.select().join(Waypoint).group_by(Route.id)
+        routes = conn()
         self.render("routes.html", routes=routes)
 
 
